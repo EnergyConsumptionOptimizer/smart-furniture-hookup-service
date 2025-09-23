d63436f6ffe5ee4b04068d8f38f4a1ae05e3f1d0
rares vasiliu <rv.vasiliu@gmail.com>
2025-09-23 16:35:37 +0200

    feat: add error handler middleware for web API endpoints
    Signed-off-by: rares vasiliu <rv.vasiliu@gmail.com>

---
+++ src/interfaces/web-api/middlewares/errorHandlerMiddleware.ts
@@ -0,0 +1,37 @@
+import { NextFunction, Request, Response } from "express";
+import {
+  InvalidIDError,
+  SmartFurnitureHookupEndpointConflictError,
+  SmartFurnitureHookupNameConflictError,
+  SmartFurnitureHookupNotFoundError,
+} from "../../../domain/errors/errors";
+
+const msgError = (message: string) => {
+  return { error: message };
+};
+
+export const errorHandler = (
+  error: Error,
+  _request: Request,
+  response: Response,
+  next: NextFunction,
+) => {
+  void next;
+
+  if (error instanceof InvalidIDError) {
+    return response.status(400).json(msgError(error.message));
+  }
+
+  if (error instanceof SmartFurnitureHookupNotFoundError) {
+    return response.status(404).json(msgError(error.message));
+  }
+
+  if (
+    error instanceof SmartFurnitureHookupNameConflictError ||
+    error instanceof SmartFurnitureHookupEndpointConflictError
+  ) {
+    return response.status(409).json(msgError(error.message));
+  }
+
+  return response.status(500).send();
+};
