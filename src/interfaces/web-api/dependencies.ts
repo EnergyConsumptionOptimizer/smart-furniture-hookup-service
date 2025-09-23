e1aceeba921d82bc88135e1fc6a6bcace968276c
rares vasiliu <rv.vasiliu@gmail.com>
2025-09-23 23:02:28 +0200

    feat: replace in-memory SmartFurnitureHookupRepository with Mongoose implementation
    Signed-off-by: rares vasiliu <rv.vasiliu@gmail.com>

--- src/interfaces/web-api/dependencies.ts
+++ src/interfaces/web-api/dependencies.ts
@@ -1,13 +1,13 @@
 import { router } from "./routes/router";
 
 import { SmartFurnitureHookupController } from "./controllers/SmartFurnitureHookupController";
-import { InMemorySmartFurnitureHookupRepository } from "../../test/storage/InMemorySmartFurnitureHookupRepository";
 import { SmartFurnitureHookupServiceImpl } from "../../application/SmartFurnitureHookupServiceImpl";
 import { AuthMiddleware } from "./middlewares/AuthMiddleware";
+import { MongooseSmartFurnitureHookupRepository } from "../../storage/mongo/MongooseSmartFurnitureHookupRepository";
 
 // ===== Repository =====
 export const smartFurnitureHookupRepository =
-  new InMemorySmartFurnitureHookupRepository();
+  new MongooseSmartFurnitureHookupRepository();
 
 // ===== Services =====
 export const smartFurnitureHookupService = new SmartFurnitureHookupServiceImpl(
