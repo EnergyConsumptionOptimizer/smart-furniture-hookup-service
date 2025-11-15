7b64d71d33376bdb9d10e2fbb6e7b24754e6b216
rares vasiliu <rv.vasiliu@gmail.com>
2025-11-15 22:13:18 +0100

    feat: add new domain errors

--- src/domain/domain/errors/errors.ts
+++ src/domain/domain/errors/errors.ts
@@ -1,3 +1,31 @@
+export class SmartFurnitureHookupNameConflictError extends Error {
+  constructor(name: string) {
+    super(`Name ${name} already exists`);
+    this.name = "SmartFurnitureHookupNameConflictError";
+  }
+}
+
+export class SmartFurnitureHookupEndpointConflictError extends Error {
+  constructor(endpoint: string) {
+    super(`Endpoint ${endpoint} already exists`);
+    this.name = "SmartFurnitureHookupEndpointConflictError";
+  }
+}
+
+export class SmartFurnitureHookupNotFoundError extends Error {
+  constructor() {
+    super("Smart furniture hookup not found");
+    this.name = "SmartFurnitureHookupNotFoundError";
+  }
+}
+
+export class InvalidIDError extends Error {
+  constructor() {
+    super("Invalid smart furniture hookup ID format");
+    this.name = "InvalidIDError";
+  }
+}
+
 export class InvalidUtilityTypeError extends Error {
   constructor(type: string) {
     super(`Invalid utility type: ${type}`);
