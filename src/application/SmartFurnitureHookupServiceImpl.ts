b486c3bc2ceb423c12f8de3398313428e66d9460
rares vasiliu <rv.vasiliu@gmail.com>
2025-11-15 23:28:36 +0100

    feat: add SmartFurnitureHookupService implementation

---
+++ src/application/SmartFurnitureHookupServiceImpl.ts
@@ -0,0 +1,68 @@
+import { SmartFurnitureHookupService } from "@domain/ports/SmartFurnitureHookupService";
+import { SmartFurnitureHookupRepository } from "@domain/ports/SmartFurnitureHookupRepository";
+import { SmartFurnitureHookup } from "@domain/SmartFurnitureHookup";
+import { utilityTypeFromString } from "@domain/UtilityType";
+import { SmartFurnitureHookupFactory } from "@domain/SmartFurnitureHookupFactory";
+import { SmartFurnitureHookupID } from "@domain/SmartFurnitureHookupID";
+import { SmartFurnitureHookupNotFoundError } from "@domain/errors/errors";
+
+export class SmartFurnitureHookupServiceImpl
+  implements SmartFurnitureHookupService
+{
+  constructor(
+    private readonly smartFurnitureHookupRepository: SmartFurnitureHookupRepository,
+  ) {}
+
+  async createSmartFurnitureHookup(
+    name: string,
+    type: string,
+    endpoint: string,
+  ): Promise<SmartFurnitureHookup> {
+    return this.smartFurnitureHookupRepository.saveSmartFurnitureHookup(
+      new SmartFurnitureHookupFactory().createSmartFurnitureHookup(
+        name,
+        utilityTypeFromString(type),
+        endpoint,
+      ),
+    );
+  }
+
+  async deleteSmartFurnitureHookup(id: SmartFurnitureHookupID): Promise<void> {
+    return this.smartFurnitureHookupRepository.removeSmartFurnitureHookup(id);
+  }
+
+  async getSmartFurnitureHookups(): Promise<SmartFurnitureHookup[]> {
+    return this.smartFurnitureHookupRepository.findAllSmartFurnitureHookup();
+  }
+
+  async getSmartFurnitureHookup(
+    id: SmartFurnitureHookupID,
+  ): Promise<SmartFurnitureHookup | null> {
+    return this.smartFurnitureHookupRepository.findSmartFurnitureHookupByID(id);
+  }
+
+  async updateSmartFurnitureHookup(
+    id: SmartFurnitureHookupID,
+    name?: string,
+    endpoint?: string,
+  ): Promise<SmartFurnitureHookup> {
+    const currentSmartFurnitureHookup =
+      await this.smartFurnitureHookupRepository.findSmartFurnitureHookupByID(
+        id,
+      );
+
+    if (!currentSmartFurnitureHookup) {
+      throw new SmartFurnitureHookupNotFoundError();
+    }
+
+    const smartFurnitureHookupToUpdate: SmartFurnitureHookup = {
+      ...currentSmartFurnitureHookup,
+      name: name ?? currentSmartFurnitureHookup.name,
+      endpoint: endpoint ?? currentSmartFurnitureHookup.endpoint,
+    };
+
+    return this.smartFurnitureHookupRepository.updateSmartFurnitureHookup(
+      smartFurnitureHookupToUpdate,
+    );
+  }
+}
