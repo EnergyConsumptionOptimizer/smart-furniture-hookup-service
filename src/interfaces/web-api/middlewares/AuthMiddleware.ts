9819d7ae1181b8f2b54ae3a37ea0c6bf1f44a9a7
rares vasiliu <rv.vasiliu@gmail.com>
2025-11-17 22:30:50 +0100

    fix: import InvalidTokenError

--- src/interfaces/web-api/middlewares/AuthMiddleware.ts
+++ src/interfaces/web-api/middlewares/AuthMiddleware.ts
@@ -1,5 +1,6 @@
 import { type NextFunction, type Request, type Response } from "express";
 import axios from "axios";
+import { InvalidTokenError } from "@interfaces/web-api/middlewares/authMiddewareErrors";
 
 export class AuthMiddleware {
   private checkToken = (request: Request) => {
