147ba7f8d4d3bd77a9c2892ac78b270372a0a83c
rares vasiliu <rv.vasiliu@gmail.com>
2025-11-18 10:37:55 +0100

    feat: add smartFurnitureHookupRoutes to webapi routes

--- src/interfaces/web-api/routes/router.ts
+++ src/interfaces/web-api/routes/router.ts
@@ -1,9 +1,18 @@
 import { Router } from "express";
+import { smartFurnitureHookupRoutes } from "@interfaces/web-api/routes/smartFurnitureHookupRoutes";
+import { AuthMiddleware } from "@interfaces/web-api/middlewares/AuthMiddleware";
+import { SmartFurnitureHookupController } from "@interfaces/web-api/controllers/SmartFurnitureHookupController";
 
-export function router(): Router {
+export function router(
+  smartFurnitureHookupController: SmartFurnitureHookupController,
+  authMiddleware: AuthMiddleware,
+): Router {
   const router = Router();
 
-  router.use("/api/smart-furniture-hookups");
+  router.use(
+    "/api/smart-furniture-hookups",
+    smartFurnitureHookupRoutes(smartFurnitureHookupController, authMiddleware),
+  );
 
   return router;
 }
