dea65ef301b43b110257d4a730fff07aca5e1cbf
rares vasiliu <rv.vasiliu@gmail.com>
2026-01-10 11:09:04 +0100

    chore: add cookie-parser dependency

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,10 @@ import express from "express";
 import { apiRouter } from "@interfaces/web-api/dependencies";
 import { errorHandler } from "@interfaces/web-api/middlewares/errorHandlerMiddleware";
 
+import cookieParser from "cookie-parser";
 const app = express();
 
+app.use(cookieParser());
 app.use(express.json());
 
 app.use(apiRouter);
