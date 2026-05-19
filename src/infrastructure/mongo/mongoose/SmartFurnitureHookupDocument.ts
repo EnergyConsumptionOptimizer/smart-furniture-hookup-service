8cbb5cdd936368da356de645a9520d4744c23017
rares vasiliu <rv.vasiliu@gmail.com>
2026-05-19 23:27:29 +0200

    refactor: improve data to/from domain in mongoose

--- src/infrastructure/mongo/mongoose/SmartFurnitureHookupDocument.ts
+++ src/infrastructure/mongo/mongoose/SmartFurnitureHookupDocument.ts
@@ -1,10 +1,10 @@
 import { type Document } from "mongoose";
-import { UtilityType } from "@domain/UtilityType";
+import { UtilityTypeEnum } from "@domain/values/UtilityType";
 
 export interface SmartFurnitureHookupDocument extends Document<string> {
   readonly _id: string;
   readonly name: string;
-  readonly utilityType: UtilityType;
+  readonly utilityType: UtilityTypeEnum;
   readonly endpoint: string;
   readonly createdAt: Date;
   readonly updatedAt: Date;
--- src/infrastructure/mongo/mongoose/SmartFurnitureHookupModel.ts
+++ src/infrastructure/mongo/mongoose/SmartFurnitureHookupModel.ts
@@ -1,5 +1,10 @@
 import { SmartFurnitureHookupDocument } from "./SmartFurnitureHookupDocument";
 import { model, Schema } from "mongoose";
+import { UtilityType, UtilityTypeEnum } from "@domain/values/UtilityType";
+import { SmartFurnitureHookup } from "@domain/entities/SmartFurnitureHookup";
+import { SmartFurnitureHookupID } from "@domain/values/SmartFurnitureHookupID";
+import { SmartFurnitureHookupName } from "@domain/values/SmartFurnitureHookupName";
+import { EndpointURL } from "@domain/values/EndpointURL";
 
 const smartFurnitureHookupSchema = new Schema<SmartFurnitureHookupDocument>(
   {
@@ -15,6 +20,7 @@ const smartFurnitureHookupSchema = new Schema<SmartFurnitureHookupDocument>(
     utilityType: {
       type: String,
       required: true,
+      enum: Object.values(UtilityTypeEnum),
     },
     endpoint: {
       type: String,
@@ -31,3 +37,28 @@ export const SmartFurnitureHookupModel = model<SmartFurnitureHookupDocument>(
   "SmartFurnitureHookup",
   smartFurnitureHookupSchema,
 );
+
+export const SmartFurnitureHookupMapper = {
+  toPersistence(domain: SmartFurnitureHookup) {
+    return {
+      _id: domain.id.toString(),
+      name: domain.name.toString(),
+      utilityType: domain.utilityType.toString(),
+      endpoint: domain.endpoint.toString(),
+    };
+  },
+
+  toDomain(
+    document: Pick<
+      SmartFurnitureHookupDocument,
+      "_id" | "name" | "utilityType" | "endpoint"
+    >,
+  ): SmartFurnitureHookup {
+    return SmartFurnitureHookup.rehydrate(
+      SmartFurnitureHookupID.from(document._id) as SmartFurnitureHookupID,
+      SmartFurnitureHookupName.from(document.name) as SmartFurnitureHookupName,
+      UtilityType.from(document.utilityType) as UtilityType,
+      EndpointURL.from(document.endpoint) as EndpointURL,
+    );
+  },
+};
