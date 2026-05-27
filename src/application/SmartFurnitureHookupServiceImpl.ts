52642f84d421442dfca6d78f3835f5ae2b40675b
rares vasiliu <rv.vasiliu@gmail.com>
2026-05-27 11:52:11 +0200

    feat: use physicalSmartFurnitureHookupCommunication to connect with the physical hookup

--- src/application/SmartFurnitureHookupServiceImpl.ts
+++ src/application/SmartFurnitureHookupServiceImpl.ts
@@ -11,10 +11,12 @@ import { SmartFurnitureHookupName } from "@domain/values/SmartFurnitureHookupNam
 import { UtilityType } from "@domain/values/UtilityType";
 import { EndpointURL } from "@domain/values/EndpointURL";
 import { SmartFurnitureHookupNotFoundError, stripErrors } from "@domain/errors";
+import { PhysicalSmartFurnitureHookupCommunication } from "@application/outbound/PhysicalSmartFurnitureHookupCommunication";
 
 export class SmartFurnitureHookupServiceImpl implements SmartFurnitureHookupService {
   readonly #repository: SmartFurnitureHookupRepository;
   readonly #monitoringService: MonitoringService;
+  readonly #physicalSmartFurnitureHookupCommunication: PhysicalSmartFurnitureHookupCommunication;
   readonly #idGenerator: IdGenerator;
   readonly #uow: UnitOfWork;
   readonly #eventPublisher: EventPublisher;
@@ -23,6 +25,7 @@ export class SmartFurnitureHookupServiceImpl implements SmartFurnitureHookupServ
   constructor(
     smartFurnitureHookupRepository: SmartFurnitureHookupRepository,
     monitoringService: MonitoringService,
+    physicalSmartFurnitureHookupCommunication: PhysicalSmartFurnitureHookupCommunication,
     idGenerator: IdGenerator,
     uow: UnitOfWork,
     eventPublisher: EventPublisher,
@@ -30,10 +33,35 @@ export class SmartFurnitureHookupServiceImpl implements SmartFurnitureHookupServ
   ) {
     this.#repository = smartFurnitureHookupRepository;
     this.#monitoringService = monitoringService;
+    this.#physicalSmartFurnitureHookupCommunication =
+      physicalSmartFurnitureHookupCommunication;
     this.#idGenerator = idGenerator;
     this.#uow = uow;
     this.#eventPublisher = eventPublisher;
     this.#metrics = metrics;
+  }
+
+  async #connectToPhysicalSmartFurnitureHookup(
+    smartFurnitureHookup: SmartFurnitureHookup,
+  ): Promise<undefined | Error> {
+    const ingestingEndpoint =
+      await this.#monitoringService.getIngestingEndpoint();
+
+    if (ingestingEndpoint instanceof Error) return ingestingEndpoint;
+
+    return this.#physicalSmartFurnitureHookupCommunication.updateIngestingEndpoint(
+      smartFurnitureHookup.endpoint.value,
+      `${ingestingEndpoint}?smart_furniture_hookup_id=${smartFurnitureHookup.id.value}`,
+    );
+  }
+
+  async #disconnectToPhysicalSmartFurnitureHookup(
+    smartFurnitureHookup: SmartFurnitureHookup,
+  ): Promise<undefined | Error> {
+    return this.#physicalSmartFurnitureHookupCommunication.updateIngestingEndpoint(
+      smartFurnitureHookup.endpoint.value,
+      "",
+    );
   }
 
   async createSmartFurnitureHookup(
@@ -57,20 +85,23 @@ export class SmartFurnitureHookupServiceImpl implements SmartFurnitureHookupServ
       props.endpoint,
     );
 
-    await this.#uow.executeTransactionally(async () => {
-      await this.#repository.saveSmartFurnitureHookup(
+    const connectionResponse =
+      await this.#connectToPhysicalSmartFurnitureHookup(
         smartFurnitureHookupEntity,
       );
 
-      await this.#monitoringService.registerSmartFurnitureHookup(
-        smartFurnitureHookupEntity.id,
-        smartFurnitureHookupEntity.endpoint,
+    if (connectionResponse instanceof Error) return connectionResponse;
+
+    await this.#uow.executeTransactionally(async () => {
+      await this.#repository.saveSmartFurnitureHookup(
+        smartFurnitureHookupEntity,
       );
 
       for (const event of smartFurnitureHookupEntity.pullDomainEvents()) {
         await this.#eventPublisher.publish(event);
       }
     });
+
     this.#metrics.recordSmartFurnitureHookupCreation();
     return smartFurnitureHookupEntity;
   }
@@ -91,16 +122,14 @@ export class SmartFurnitureHookupServiceImpl implements SmartFurnitureHookupServ
     smartFurnitureHookup.prepareForDeletion();
 
     await this.#uow.executeTransactionally(async () => {
-      await this.#monitoringService
-        .disconnectSmartFurnitureHookup(smartFurnitureHookup.endpoint)
-        .catch();
-
       await this.#repository.removeSmartFurnitureHookup(props.id);
 
       for (const event of smartFurnitureHookup.pullDomainEvents()) {
         await this.#eventPublisher.publish(event);
       }
     });
+
+    await this.#disconnectToPhysicalSmartFurnitureHookup(smartFurnitureHookup);
 
     this.#metrics.recordSmartFurnitureHookupDeletion();
   }
@@ -147,23 +176,17 @@ export class SmartFurnitureHookupServiceImpl implements SmartFurnitureHookupServ
       return smartFurnitureHookup;
     }
 
-    if (props.endpoint) smartFurnitureHookup.changeEndpoint(props.endpoint);
+    if (props.endpoint) {
+      smartFurnitureHookup.changeEndpoint(props.endpoint);
+      const connectionResponse =
+        await this.#connectToPhysicalSmartFurnitureHookup(smartFurnitureHookup);
+
+      if (connectionResponse instanceof Error) return connectionResponse;
+    }
+
     if (props.name) smartFurnitureHookup.changeName(props.name);
 
-    await this.#uow.executeTransactionally(async () => {
-      if (endpoint) {
-        await this.#monitoringService.registerSmartFurnitureHookup(
-          smartFurnitureHookup.id,
-          smartFurnitureHookup.endpoint,
-        );
-      }
-
-      await this.#repository.updateSmartFurnitureHookup(smartFurnitureHookup);
-
-      for (const event of smartFurnitureHookup.pullDomainEvents()) {
-        await this.#eventPublisher.publish(event);
-      }
-    });
+    await this.#repository.updateSmartFurnitureHookup(smartFurnitureHookup);
 
     this.#metrics.recordSmartFurnitureHookupUpdate();
 
